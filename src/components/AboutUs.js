1d74f2df451749cf60e11aa72e2a180eff7a8db7
grace <gbu@my.bcit.ca>
2021-08-30 20:28:26 -0700

    Updated the about us info again.

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -8,19 +8,21 @@ const AboutUs = () => {
 
       <div className="AboutUs">
         <p className="about-details">
-                  Hello Everyone!<br />
-                  We are a student run club at BCIT. <br />
-                  Our club's main goal is to<br />
-                  <ul>
-                      <li>Empower women in a male-dominated industry</li>
-                      <li>Promote women in Computing</li>
-                      <li>Connect students with female professionals in the Vancouver Tech Industry</li>
-                  </ul>
+                  Hello Everyone!
+                  <br />
+                  <br />
+                  We are a student run club at BCIT. This club was started by a group of like-minded individuals who wanted to create a safe space for the female students in Computing programs at BCIT.
+                  <br />
+                  <br />
+                  Our club's main goal is to Empower women in a male-dominated industry, Promote women in Computing, and Connect students with female professionals in the Vancouver Tech Industry.
+                  <br />
                   <br />
                   We host events and collabrate with local companies to bring tips & tricks and networking opportunities to our club members.
                   Such as, Meet and Greets, Resume & Cover Letter Mini Panels and Game Nights.
                   <br />
+                  <br />
                   We welcome all students that support our goals and want to create a safe space for like-minded people.
+                  <br />
                   <br />
                   Please email us if you have any inquires. Thank you!
         </p>
