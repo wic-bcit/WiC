6891aa6bff8fad3ec128eccb7fbfcd66fc264909
njingco <43453127+njingco@users.noreply.github.com>
2022-06-01 15:06:30 -0700

    Updates

---
+++ precache-manifest.affcb5a7e86a8288fac45d8041e823a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "efbbfb1983c79cd556ea",
+    "url": "/WiC/static/css/main.bc941bc2.chunk.css"
+  },
+  {
+    "revision": "efbbfb1983c79cd556ea",
+    "url": "/WiC/static/js/main.efbbfb19.chunk.js"
+  },
+  {
+    "revision": "4567e6b4a7bd4ab0b748",
+    "url": "/WiC/static/js/1.4567e6b4.chunk.js"
+  },
+  {
+    "revision": "dacb00947eb9e0b5ff3a",
+    "url": "/WiC/static/js/2.dacb0094.chunk.js"
+  },
+  {
+    "revision": "8bd291d91478e9bb0f17",
+    "url": "/WiC/static/js/runtime~main.8bd291d9.js"
+  },
+  {
+    "revision": "c0b6b88a0a724c066eceab75e8792dca",
+    "url": "/WiC/index.html"
+  }
+];
