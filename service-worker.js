93e06b99f1c06c8e25aea3fc64ef52242839f722
njingco <43453127+njingco@users.noreply.github.com>
2022-06-01 17:10:18 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/WiC/precache-manifest.2b4ea55818d47b0b4a2e25b2b48f6727.js"
+  "/WiC/precache-manifest.09afc14a1274a0d9713447f6af3681c2.js"
 );
 
 workbox.clientsClaim();
