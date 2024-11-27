0744fbcdd45badcc75b6261c8f22806aaa67f141
Angela Yu <ayu107@my.bcit.ca>
2024-11-27 00:37:12 -0800

    try

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,4 +1,4 @@
-export const home_route = "/";
+export const home_route = process.env.PUBLIC_URL + "/";
 export const events_route = "/events-page";
 export const team_route = "/team-page";
 export const blog_route = "/blog-page";
