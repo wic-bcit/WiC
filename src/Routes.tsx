20fe184614892c284e69ca9134aa6ffe6ec82ff1
Angela Yu <ayu107@my.bcit.ca>
2024-11-27 01:05:34 -0800

    try 3

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,3 +1,3 @@
 export const home_route = process.env.PUBLIC_URL + "/";
-export const events_route = "/events-page";
-export const blog_route = "/blog-page";
+export const events_route = process.env.PUBLIC_URL + "/events-page";
+export const blog_route = process.env.PUBLIC_URL + "/blog-page";
