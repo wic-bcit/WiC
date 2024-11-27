c362870b9afd3d41df3e52317aded18182cb4a79
Angela Yu <ayu107@my.bcit.ca>
2024-11-27 00:04:02 -0800

    try this route

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,4 +1,4 @@
-export const home_route = "/WiC";
+export const home_route = "/";
 export const events_route = "/events-page";
 export const team_route = "/team-page";
 export const blog_route = "/blog-page";
